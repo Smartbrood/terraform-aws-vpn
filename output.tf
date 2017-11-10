4d0088c0bc89b45f0fa671a2dd2e3dd425465fc6
Andrey N. Petrov <andreynpetrov@gmail.com>
2017-11-10 18:43:14 +0300

    auto intermediate

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "customer_gateway_configuration" {
   description = "The configuration information for the VPN connection's customer gateway"
-  value = "${aws_vpn_connection.this.customer_gateway_configuration}"
+  value = "${aws_vpn_connection.this.*.customer_gateway_configuration}"
 }
 
 
